b8f7e47693696d199350361b39819ccdda52e41d
Oscar Andres Gomez <os.gomez.lozano@gmail.com>
2022-10-06 20:32:11 -0500

    Some unfinished extras

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,5 +1,16 @@
 package model;
 
 public class Game {
+    public final static int LEVELS_INITIAL_SIZE = 10;
+    private Level[] levels;
+
+    public String addLevels(Level level) {
+        return "";
+    }
     
+    public void generateInitialLevels() {
+        for (int i = 0; i < LEVELS_INITIAL_SIZE; i++) {
+            
+        }
+    }
 }
